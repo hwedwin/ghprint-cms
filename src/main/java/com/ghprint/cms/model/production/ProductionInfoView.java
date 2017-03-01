ad8d1b497da5e6da5a65a68cb8530c66e83d262c
lipeiting <597836206@qq.com>
2017-03-01 10:15:41 +0800

    20170301:框架优化，新增质量标准模块初始化model

---
+++ src/main/java/com/ghprint/cms/model/production/ProductionInfoView.java
@@ -0,0 +1,73 @@
+package com.ghprint.cms.model.production;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+public class ProductionInfoView implements Serializable {
+
+    private TProductionStandard productionStandard;
+
+    private  TCustomerInfo customerInfo;
+
+    private  TProductInfo productInfo;
+
+    private  TPrintingData printingData;
+
+    private  TPrintingProcedure printingProcedure;
+
+    private  TPrintingProcedureafter printingProcedureafter;
+
+    public  ProductionInfoView (){}
+
+    public TProductionStandard getProductionStandard() {
+        return productionStandard;
+    }
+
+    public void setProductionStandard(TProductionStandard productionStandard) {
+        this.productionStandard = productionStandard;
+    }
+
+    public TCustomerInfo getCustomerInfo() {
+        return customerInfo;
+    }
+
+    public void setCustomerInfo(TCustomerInfo customerInfo) {
+        this.customerInfo = customerInfo;
+    }
+
+    public TProductInfo getProductInfo() {
+        return productInfo;
+    }
+
+    public void setProductInfo(TProductInfo productInfo) {
+        this.productInfo = productInfo;
+    }
+
+    public TPrintingData getPrintingData() {
+        return printingData;
+    }
+
+    public void setPrintingData(TPrintingData printingData) {
+        this.printingData = printingData;
+    }
+
+    public TPrintingProcedure getPrintingProcedure() {
+        return printingProcedure;
+    }
+
+    public void setPrintingProcedure(TPrintingProcedure printingProcedure) {
+        this.printingProcedure = printingProcedure;
+    }
+
+    public TPrintingProcedureafter getPrintingProcedureafter() {
+        return printingProcedureafter;
+    }
+
+    public void setPrintingProcedureafter(TPrintingProcedureafter printingProcedureafter) {
+        this.printingProcedureafter = printingProcedureafter;
+    }
+
+
+}
